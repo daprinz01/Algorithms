167703bdd48681064ac69b17497a4e589c11ea42
Prince Okechukwu <daprinz.op@gmail.com>
2022-06-05 15:02:13 +0100

    * .gitignore: Added sort algorithm with test data
    * IntroSort.sln:
    * Program.cs:
    * IntroSort.csproj:
    * IntroSort.csproj.user:
    * AssemblyInfo.cs:

---
+++ IntroSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntroSort
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 23, 12, 23, 235, 58398, 234, 89, 99, 90, 22, 1, 0, 3489, 123 };
+            Console.WriteLine(string.Join(",", array));
+            IntroSort.Sort.IntroSortAlgorithm.IntroSort(ref array);
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
